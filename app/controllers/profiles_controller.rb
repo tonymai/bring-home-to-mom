753da49a3cd3126204179f4bda7ce9555813c81f
Sofie Garden & Tony Mai <pair+slgarden.tonymai@devbootcamp.com>
2015-04-17 12:07:03 -0700

    Set up controller and view (form) to create new child profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,43 @@
 class ProfilesController < ApplicationController
+  def index
+  end
+
+  def new
+    @parent = Parent.find(params[:user_id])
+    @child = Child.new
+  end
+
+  def create
+    @parent = Parent.find(params[:user_id])
+    @child = @parent.children.new(profile_params)
+    if @child.save
+      redirect_to user_path
+    else
+      render 'profiles/new'
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:child).permit(
+      :first_name,
+      :last_name,
+      :city,
+      :state,
+      :phone,
+      :gender,
+      :sexual_preference,
+      :religion,
+      :birthdate,
+      :bio,
+      :smoke,
+      :linkedin_url,
+      :facebook_url,
+      :pf_image_1,
+      :pf_image_2,
+      :pf_image_3,
+      :pf_image_4,
+      :pf_image_5)
+  end
 end
