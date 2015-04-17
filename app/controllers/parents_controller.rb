02c5e10602115157b1ac6a8c69623a0d6d9d5dcf
Sofie Garden & Tony Mai <pair+slgarden.tonymai@devbootcamp.com>
2015-04-17 11:24:10 -0700

    Update devise routes and add routes for profiles

--- app/controllers/parents_controller.rb
+++
@@ -1,5 +0,0 @@
-class ParentsController < ApplicationController
-
-
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :parents
+  devise_for :users, class_name: 'Parent'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +17,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :users, only: [:index, :show] do
+    resources :profiles
+  end
 
   # Example resource route with options:
   #   resources :products do
