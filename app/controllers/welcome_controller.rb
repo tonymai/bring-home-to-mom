3c0cb44b8688c6c89590d6727afcbd1c7c1912d0
Jonathan Berk & Kevin C <pair+berkjon.sollabeck@devbootcamp.com>
2015-04-17 19:03:08 -0700

    get signup form working on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    # redirect_to "/users/#{current_user.id}/profiles" if user_signed_in?
+    puts session["flash"]["flashes"]["notice"]
+    # byebug
   end
+
 
 end
