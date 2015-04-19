2c3916651ca7d9586912a974c29385f1c69d82fd
Apprentice <thetonymai@gmail.com>
2015-04-19 13:25:40 -0700

    Display only individuals of same orientation

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('page:change', function() {
-
+  debugger
 
   var updateMatches = function() {
     var user_filters = $.map($("select.filterby").toArray(), function(e){return $(e).val();});
@@ -9,12 +9,13 @@ $(document).on('page:change', function() {
       gender: user_filters[0],
       religion: user_filters[1],
       smoke: user_filters[2],
+      childGender: $("h1").attr("data-gender")
     }
     $.ajax({
       url: '/filter',
       type: 'GET',
       dataType: 'JSON',
-      data: {filterData},
+      data: {filterData: filterData},
     })
     .done(function(response) {
       var source = $("#child_preview_template").html();
--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -2,10 +2,16 @@ class FilterController < ApplicationController
   include FilterConcern
 
   def filter_matches
-    #refactor this by initializing hash with smoke = false and religion = nil
+
     params[:filterData].delete('gender') if params[:filterData]['gender'] == "both"
+    child_gender = params[:filterData]['childGender']
+    params[:filterData].delete('childGender')
     matches = Child.where(process_filters(params))
-    # match_ids = matches.map{|child| child.id}
-    render json: {matches: matches}
+
+    nonincestual_matches = matches.select{|child| child.parent != current_user}
+
+    same_orientation = nonincestual_matches.select{|child| child.sexual_preference = child_gender}
+
+    render json: {matches: nonincestual_matches}
   end
 end
