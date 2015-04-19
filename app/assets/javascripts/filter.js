b5a63e62b6aa63fe5eb80636643a5084e9a591dc
Kevin C <kevin.ceballos@gmail.com>
2015-04-18 23:22:26 -0700

    add handlebars and DRYed up AJAX

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -2,39 +2,14 @@
 // All this logic will automatically be available in application.js.
 $(document).on('page:change', function() {
 
-  var user_filters = $.map($("select.filterby").toArray(), function(e){return $(e).val();});
-  var filterData = {
-    gender: user_filters[0],
-    religion: user_filters[1],
-    smoke: user_filters[2],
-  }
-  $.ajax({
-    url: '/filter',
-    type: 'GET',
-    dataType: 'JSON',
-    data: {filterData},
-  })
-  .done(function(response) {
-    $("tr").fadeOut('slow', function() {
-      $.each(response.match_ids, function(index, value){
-        $("tr#" + value).show();
-      });
-    });
 
-  })
-
-
-  $('.filterby').change(function(){
-    //refactor this...
+  var updateMatches = function() {
     var user_filters = $.map($("select.filterby").toArray(), function(e){return $(e).val();});
     var filterData = {
       gender: user_filters[0],
       religion: user_filters[1],
       smoke: user_filters[2],
     }
-
-
-
     $.ajax({
       url: '/filter',
       type: 'GET',
@@ -42,15 +17,24 @@ $(document).on('page:change', function() {
       data: {filterData},
     })
     .done(function(response) {
-      $("tr").hide();
-      $.each(response.match_ids, function(index, value){
-          $("tr#" + value).show();
-      });
-    })
+      var source = $("#child_preview_template").html();
+      var templatingFunction = Handlebars.compile(source);
+      var context = {};
+      context.match = response.matches;
+      $(".matches").text("");
+      $(".matches").append(templatingFunction(context));
+    });
+  };
+
+  updateMatches();
+
+  $('.filterby').change(updateMatches);
 
 
     // $("div.matches").find("tr").hide();
     // $("div.matches").find("div." + filters).show();
-  })
 
 });
+
+
+
--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -5,7 +5,7 @@ class FilterController < ApplicationController
     #refactor this by initializing hash with smoke = false and religion = nil
     params[:filterData].delete('gender') if params[:filterData]['gender'] == "both"
     matches = Child.where(process_filters(params))
-    match_ids = matches.map{|child| child.id}
-    render json: {match_ids: match_ids}
+    # match_ids = matches.map{|child| child.id}
+    render json: {matches: matches}
   end
 end
