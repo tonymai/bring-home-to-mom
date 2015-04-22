6d674b76905150937d1194fd9d470e40f0c4b88c
sollabeck <kevin.ceballos@gmail.com>
2015-04-22 11:06:01 -0700

    Add filler navbar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
     if user_signed_in?
       if current_user.children.length > 0
     	session[:profile_id] = current_user.children.last unless session[:profile_id]
-      @current_child = Child.find(session[:profile_id])
+      @current_child = Child.find(session[:profile_id]["id"])
       else
         redirect_to new_user_profile_path(current_user.id)
       end
