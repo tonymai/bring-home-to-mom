94d304999884268e99dd813668a3b7fe2ba70492
Jonathan Berk <jdsberk@gmail.com>
2015-04-24 15:31:34 -0700

    Commented out awesome_print

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 #Awesomeprint
-require 'awesome_print'
+# require 'awesome_print'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
